3ed8640a8bdf10ee23261239286229ea9ea1239c
Luka-Git-Hub <qoqorishvililuka6@gmail.com>
2025-02-16 22:04:38 +0400

    first commit

---
+++ day 00/lesson 2.py
@@ -0,0 +1,10 @@
+print(type(13)) #int
+print(type("Luka")) #str
+print(type("Qoqorishvili")) #str
+print(type(1.75)) #float
+knows_programming=False
+print(type(knows_programming)) #boolean
+
+sum1= "1"
+gw= "23"
+print(sum1 + gw)
---
+++ day 1/Classwork/index.py
@@ -0,0 +1,4 @@
+print("int-მთელი რიცხვები")
+print("float-წილადები მაგ:2,5")
+print("str-ყველაფერი რაც ბრჭყალებშია")
+print("boolean-true,false")
