b3757d02c05fdccaed1cc88398a41b9a0e8ea83d
Luka-Git-Hub <qoqorishvililuka6@gmail.com>
2025-03-09 11:13:02 +0400

    l

---
+++ day 04/classwork/clas.py
@@ -0,0 +1,4 @@
+Name = "luka"
+Age =13
+name =  Name + "!"
+age = Age + 1
---
+++ day 04/homework/homework1.py
@@ -0,0 +1,27 @@
+1
+x = 5
+y = 9
+z = 3
+print("x + y + z")
+2
+num1 = 5
+num2 = 2
+num3 = 3
+print(5 + 2 + 3)
+3
+name = "luka "
+age = 13
+age = age + 3
+print(name + str(age))
+4
+#numb4 = 3
+#numb5 = 5
+#numb6 = 9
+#print(numb4 + numb5 + numb6)
+numb4 = 3
+numb5 = 2
+numb6 = 6
+print(numb4 + numb5 + numb6)
+5
+number = 5
+print("result: " + str(number))
