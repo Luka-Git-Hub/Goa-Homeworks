f1365a899ccd3596f4a867057962828fe811b0c9
Luka-Git-Hub <qoqorishvililuka6@gmail.com>
2025-03-19 20:47:19 +0400

    p

---
+++ day06/classwork.py/class.py
@@ -0,0 +1,13 @@
+#age = input("pls input your age: ")
+#print(age)
+
+#print("hello luka")
+#name = input("luka")
+#print(name)
+
+#input("input a number")
+#print(str(13+15*2-4))
+
+name = "Luka "
+age = 13
+print("hello " + name + "you are " + str(age))
