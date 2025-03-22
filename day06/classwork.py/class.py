da8328ca11163037fc3b07746a99be7781e8c884
Luka-Git-Hub <qoqorishvililuka6@gmail.com>
2025-03-23 01:11:13 +0400

    h

--- day06/classwork.py/class.py
+++ day06/classwork.py/class.py
@@ -8,6 +8,6 @@
 #input("input a number")
 #print(str(13+15*2-4))
 
-name = "Luka "
-age = 13
-print("hello " + name + "you are " + str(age))
+#name = "Luka "
+#age = 13
+#print("hello " + name + "you are " + str(age))
---
+++ day06/homework.py/classwork.py
@@ -0,0 +1,18 @@
+#1
+# number = input("input any number: ")
+# print(number)
+#2
+# name = input("please input your name: ")
+# print(name)
+# print(type(str(name)))
+#3
+# print(int("15"))
+#4
+# type() ფუნქცია ამოწმებს რა ტიპისაა მაგ.
+# name ანუ ის შეიძლება იყოს string , integer ან float ტიპის
+#5
+# num1 = input("input first number: ")
+# num2 = input("input second number: ")
+# print(int(num1) + int(num2))
+#6
+# inputი მომხმარებელს აძლევს საშვალებას შემოიტანოს რაიმე
