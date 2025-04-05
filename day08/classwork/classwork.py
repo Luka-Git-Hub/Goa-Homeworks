b15517b5907912f1b6f1b8113bb1e3cd999aff14
Luka-Git-Hub <qoqorishvililuka6@gmail.com>
2025-04-05 21:56:26 +0400

    hffg

---
+++ day08/classwork/classwork.py
@@ -0,0 +1,15 @@
+# 2)
+# num1 = input("please input first number ")
+# num2 = input("please input second number ")
+
+# print(num1 < num2)
+
+# 1)
+# num1 = in
+
+my_height = float(input("შეიყვანეთ თქვენი სიმაღლე: "))
+friend_height = float(input("შეიყვანეთ თქვენი მეგობრის სიმაღლე: "))
+
+print("you are taller than your friend: ", my_height > friend_height)
+print("your friend is taller than you: ", friend_height > my_height)
+print("You and your friend are the same height: ", my_height == friend_height)
