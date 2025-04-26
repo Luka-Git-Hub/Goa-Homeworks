f776a80f8d53d98c39dee98a99bdae065f6978f3
Luka-Git-Hub <qoqorishvililuka6@gmail.com>
2025-04-26 20:52:30 +0400

    git

---
+++ day 10/classwork/classwork.py
@@ -0,0 +1,5 @@
+# print(True and False or False and False or True or True or False)
+
+# 0) 1 უნდა შევასრულოთ and ეს კი falseს გამოიტანს ანუ თუ ორივე true არ არის andში მაშინ falseს გამოიტანს
+# 1) 2 უნდა შევასრულოთ or ეს კი trueს გამოიტანს ანუ თუ ერთი მაინც true არის trueს გამოიტანს
+# პასუხი = true
---
+++ day 10/homework/homework.py
@@ -0,0 +1,2 @@
+print(True and not False and (False or True) and True or not False)
+#          true          and       true               or     true
