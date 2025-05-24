35f0182fcb1965ddfda5d6584fc65c7fe2f7deb4
Luka-Git-Hub <qoqorishvililuka6@gmail.com>
2025-05-24 14:54:59 +0400

    supa

---
+++ day 14/class/class.py
@@ -0,0 +1,17 @@
+age = int(input("შეიყვანე ასაკი: "))
+experience = int(input("შეიყვანე გამოცდილება წლებში: "))
+height = int(input("შეიყვანე სიმაღლე: "))
+
+isHired = age >= 18 and experience >= 2 and height >= 175
+
+print("You are hired:", isHired)
+
+
+people = int(input("შეიყვანე ადამიანების რაოდენობა: "))
+time = int(input("შეიყვანე დრო 1-დან 12-მდე: "))
+switch = input("ჩართვა true ან false: ") == "true"
+
+lightsOn = people > 2 and time >= 7 and switch
+
+print(lightsOn)
+
---
+++ day 14/homework/home.py
@@ -0,0 +1,21 @@
+#  1
+
+#  > - მეტია
+#  < - ნაკლბია
+#  >= - მეტია თუ ტოლია
+#  <= - ნაკლებია თუ ტოლია
+
+#  2
+
+# and
+# not
+# or
+
+# 3
+
+# print(5 + 10 <= 2 + 13)
+
+# 4
+
+print(True and not False and (False or True) and True or not False and False and not True and (True or False) or (True and False))
+#           true          true                true     true         false      false        false              false
