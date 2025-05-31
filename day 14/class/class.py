1d9933201a291b08a18a91df7ab21ea362dc0b8f
Luka-Git-Hub <qoqorishvililuka6@gmail.com>
2025-06-01 01:03:21 +0400

    supa

--- day 14/class/class.py
+++ day 14/class/class.py
@@ -1,17 +1,17 @@
-age = int(input("შეიყვანე ასაკი: "))
-experience = int(input("შეიყვანე გამოცდილება წლებში: "))
-height = int(input("შეიყვანე სიმაღლე: "))
+# 1)
+# age = int(input("შეიყვანე ასაკი: "))
+# experience = int(input("შეიყვანე გამოცდილება წლებში: "))
+# height = int(input("შეიყვანე სიმაღლე: "))
 
-isHired = age >= 18 and experience >= 2 and height >= 175
+# isHired = age >= 18 and experience >= 2 and height >= 175
 
-print("You are hired:", isHired)
+# print("You are hired:", isHired)
 
+# 2)
+# people = int(input("შეიყვანე ადამიანების რაოდენობა: "))
+# time = int(input("შეიყვანე დრო 1-დან 12-მდე: "))
+# switch = input("ჩართვა true ან false: ") == "true"
 
-people = int(input("შეიყვანე ადამიანების რაოდენობა: "))
-time = int(input("შეიყვანე დრო 1-დან 12-მდე: "))
-switch = input("ჩართვა true ან false: ") == "true"
+# lightsOn = people > 2 and time >= 7 and switch
 
-lightsOn = people > 2 and time >= 7 and switch
-
-print(lightsOn)
-
+# print(lightsOn)
--- day 14/homework/home.py
+++ day 14/homework/home.py
@@ -18,4 +18,14 @@
 # 4
 
 print(True and not False and (False or True) and True or not False and False and not True and (True or False) or (True and False))
-#           true          true                true     true         false      false        false              false
+print(True and not False and (False or True) and True or not False and False and not True and (True or False) or False)
+print(True and not False and (False or True) and True or not False and False and not True and True or False)
+print(True and not False and True and True or not False and False and not True and True or False)
+print(True and True and True or not False and False and not True and True or False)
+print(True and True or not False and False and not True and True or False)
+print(True or not False and False and not True and True or False)
+print(True or False and not True and True or False)
+print(True or False and True or False)
+print(True or False or False)
+print(True or False)
+print(True)
