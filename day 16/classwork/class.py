584f06afda3ba051adbf6e81c3d8c58ce9a7261f
Luka-Git-Hub <qoqorishvililuka6@gmail.com>
2025-06-14 18:48:52 +0400

    sop

--- day 16/classwork/class.py
+++ day 16/classwork/class.py
@@ -1,9 +1,19 @@
 # 1)
-# for i in range(1, 31):
-#     print(i)
+for i in range(1, 31):
+    print(i)
 
 # 2)
 age = 15
 for i in range(85):
     age = age + 1
     print(age)
+
+# 3) 
+for i in range(25, 101, 3):
+    print(i)
+
+# 4)
+age = 10
+while age < 25:
+    age = age + 1
+    print("+1 age") 
